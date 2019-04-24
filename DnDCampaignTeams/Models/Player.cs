3557894396155fc8d5ed4333adda4a1f03735ba0
Gehnster <gehnster@withineveonline>
2019-04-23 21:38:02 -0600

    Final testing and fixes.

--- DnDCampaignTeams/Models/Player.cs
+++ DnDCampaignTeams/Models/Player.cs
@@ -8,9 +8,12 @@ namespace DnDCampaignTeams.Models
     {
         public int Id { get; set; }
         [Required]
+        [Display(Name ="First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         [StringLength(37, MinimumLength = 2, ErrorMessage = "Discord Handle should be between 2 and 37 characters long")]
+        [Display(Name = "Discord Handle")]
         public string DiscordHandle { get; set; }
         public List<Character> Characters { get; set; }
     }
